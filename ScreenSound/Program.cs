742c0f4ce08e6e840f166a670508f803cd25b4f1
JasonLuis <jason20luis@gmail.com>
2024-02-14 17:49:26 -0300

    parte 1 do curso de persistencia de dados com c#

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,78 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+try
+{
+    var artistaDAL = new ArtistaDAL();
+    artistaDAL.Adicionar(new Artista("Coral Kemuel", "Antes era bom, agora ta mais que ruim."));
+    artistaDAL.Atualizar(new Artista("Sampa Crew", "Era para ser o Sorriso Maroto, mas são ruim do mesmo jeito") { Id = 2});
+    artistaDAL.Remover(2005);
+
+    var listaArtista = artistaDAL.Listar();
+
+    foreach ( var artista in listaArtista)
+    {
+        Console.WriteLine(artista);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+return; 
+Artista ira = new Artista("Ira!", "Banda Ira!");
+Artista beatles = new("The Beatles", "Banda The Beatles");
+
+Dictionary<string, Artista> artistasRegistrados = new();
+artistasRegistrados.Add(ira.Nome, ira);
+artistasRegistrados.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarArtista());
+opcoes.Add(2, new MenuRegistrarMusica());
+opcoes.Add(3, new MenuMostrarArtistas());
+opcoes.Add(4, new MenuMostrarMusicas());
+opcoes.Add(-1, new MenuSair());
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar um artista");
+    Console.WriteLine("Digite 2 para registrar a música de um artista");
+    Console.WriteLine("Digite 3 para mostrar todos os artistas");
+    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+    {
+        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(artistasRegistrados);
+        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    } 
+    else
+    {
+        Console.WriteLine("Opção inválida");
+    }
+}
+
+ExibirOpcoesDoMenu();
