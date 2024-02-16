22ba05ddb27eb2f4f8bf087d9b8fb03322d60839
JasonLuis <jason20luis@gmail.com>
2024-02-16 16:06:24 -0300

    parte 5 - Curso de Persistência de dados com C#

--- ScreenSound/Menus/MenuRegistrarMusica.cs
+++ ScreenSound/Menus/MenuRegistrarMusica.cs
@@ -5,20 +5,23 @@ namespace ScreenSound.Menus;
 
 internal class MenuRegistrarMusica : Menu
 {
-    public override void Executar(DAL<Artista> musicaDAL)
+    public override void Executar(DAL<Artista> artistaDAL)
     {
-        base.Executar(musicaDAL);
+        base.Executar(artistaDAL);
         ExibirTituloDaOpcao("Registro de músicas");
         Console.Write("Digite o artista cuja música deseja registrar: ");
         string nomeDoArtista = Console.ReadLine()!;
-        var artistaRecuperado = musicaDAL.RecupearPor(n => n.Nome.Equals(nomeDoArtista));
+        var artistaRecuperado = artistaDAL.RecupearPor(n => n.Nome.Equals(nomeDoArtista));
         if (artistaRecuperado is not null)
         {
             Console.Write("Agora digite o título da música: ");
             string tituloDaMusica = Console.ReadLine()!;
+            Console.Write("Agora digite o ano de lançamento da música: ");
+            string anoLancamento = Console.ReadLine()!;
 
-            artistaRecuperado.AdicionarMusica(new Musica(tituloDaMusica));
+            artistaRecuperado.AdicionarMusica(new Musica(tituloDaMusica) { AnoLancamento = Convert.ToInt32(anoLancamento)});
             Console.WriteLine($"A música {tituloDaMusica} de {nomeDoArtista} foi registrada com sucesso!");
+            artistaDAL.Atualizar(artistaRecuperado);
             Thread.Sleep(4000);
             Console.Clear();
         }
