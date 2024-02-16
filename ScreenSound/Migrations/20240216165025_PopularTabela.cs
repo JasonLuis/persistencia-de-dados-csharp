289c0e4da5c0e21c9b8e505cac1346534b9c24a1
JasonLuis <jason20luis@gmail.com>
2024-02-16 15:02:54 -0300

    parte 4 - Curso de Persistência de dados com C#

---
+++ ScreenSound/Migrations/20240216165025_PopularTabela.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Fernanda Brum", "Fernanda Brum Costa da Cruz, mais conhecida como Fernanda Brum, é uma cantora, escritora, atriz, apresentadora, compositora, produtora e pastora, relacionada à música cristã contemporânea brasileira.", "https://www.vagalume.com.br/fernanda-brum/images/fernanda-brum.jpg" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" }, new object[] { "Biguinho Sensação", "O músico é de Santarém, da comunidade Quilombo do Bom Jardim e já tinha um certo sucesso na região. Nas redes sociais seus vídeos bateram mais de mil visualizações se tornando um grande sucesso e até fazendo parte de memes com suas letras românticas e de sofrência.", "https://cdns-images.dzcdn.net/images/cover/660d4fc6960b122ad589905e2e09c4a7/264x264.jpg" });
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Artistas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20240216174433_PopularTabelaMusica.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Meu Xodó", 2023});
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Volto A Sorrir", 2023 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Arruma a Mala", 2023 });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Espírito Santo", 2002 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Cura-me", 2008 });
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "O Que Tua Glória Fez Comigo", 2015 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
